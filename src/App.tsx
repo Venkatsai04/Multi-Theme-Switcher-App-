890f23cbaa118c59cbca6fd7e01d292f64e0cdc8
venkat sai <saik87630@gmail.com>
2025-07-31 18:55:11 +0530

    added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+
+  return (
+    <>
+      <div className='bold font-light from-neutral-950'>
+        hi
+      </div>
+      <img src="vite.svg" alt="h" />
+    </>
+  )
+}
+
+export default App
